27f2800b3970fc74b70cbdc49e9576943e67a055
Yuvraj Singh <mail.yuvrajsingh23@gmail.com>
2024-12-24 20:30:44 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  const headingClasses = 'text-center text-3xl text-red-500 font-bold underline';
+  
+  return (
+    <h1 className={headingClasses}>
+      Vite + React + TS
+    </h1>
+  );
+}
